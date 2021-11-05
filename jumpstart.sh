c0dc21c26c85365a6d9cd0598af530b681541342
dreamscached <dreamscache.d@gmail.com>
2021-11-05 16:53:02 +0100

    Refactor; add colors

--- jumpstart.sh
+++ jumpstart.sh
@@ -4,41 +4,107 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 
-set -e
+dir="$1"
 
-
-printf "%s" "Patching DDLC.py... "
-cat <<EOF | patch "$1/DDLC.py" >/dev/null
-52a53
->     return gamedir + "/saves"
-EOF
-echo "done."
-
-
-printf "%s" "Adding console... "
-cat > "$1/game/dev_console.rpy" <<EOF
-init python:
-    config.console = True
-EOF
-echo "done."
-
-
-printf "%s" "Adding expressions previewer... "
-curl -sL "https://raw.githubusercontent.com/Monika-After-Story/MonikaModDev/master/Monika%20After%20Story/game/dev/dev_exp_previewer.rpy" > "$1/game/dev_exp_previewer.rpy"
-echo "done."
-
-
-if [ -f ~/".renpy/Monika After Story/persistent" ]; then
-  read -p "Do you want to copy existing persistent? (Y/n) " ok
-  case "$ok" in
-    "Y"|"y"|"")
-      printf "%s" "Copying persistent... "
-      mkdir -p "$1/game/saves"
-      cp ~/".renpy/Monika After Story/persistent" "$1/game/saves"
-      echo "done,"
-      ;;
-  esac
+if [ -t 1 ]; then
+  F() { printf "\033[%sm" "$1"; }
+  RE="$(F 0)"
+  BO="$(F 1)"
+  DI="$(F 2)"
+  GN="$(F "38;5;40")"
+  FV="$(F "38;5;129")"
+  RD="$(F "38;5;196")"
+  MG="$(F "38;5;207")"
+  GR="$(F "38;5;239")"
+  LG="$(F "38;5;249")"
 fi
 
 
-echo "Done."
+echo
+echo
+printf "$MG%s$RE\n"                 "               Jumpstart, a Monika After Story dev install boostrap tool."
+printf "$MG%s $BO$RD♥$RE$MG %s$R\n" "                       Made with" "by friends of Monika."
+echo
+echo
+
+
+prompt_yes_no() {
+  printf "\n\033[1A"
+  printf "  $BO$FV?$RE $BO$MG%s $RE$BO$GR(Y/n) $RE" "$1"
+  read ok
+
+  case "$ok" in
+    "")
+      printf "\033[1A\r\033[K  $BO$FV?$RE $BO$MG%s $RE$BO$GR(Y/n) ${RE}Y\n" "$1"
+      return 0;;
+    "Y"|"y"|"")
+      return 0;;
+    *)
+      return 1;;
+  esac
+}
+
+
+error_done_wrap() {
+  desc="$1"
+  printf "  $BO$LG🛠$RE $BO$MG%s... $RE" "$desc"
+  cmd="$2"
+  shift 2
+  if ! err="$("$cmd" "$@" 3>&2 2>&1 1>&3)"; then
+    printf "\r\033[K  $BO$RD✘$RE $BO$MG%s... $RE" "$desc"
+    printf "$BO${RD}error!\n\n    %s$RE\n\n" "$err"
+    printf "  $BO$RD✘ %s$RE\n\n" "Patching failed!"
+    exit 1
+  else
+    printf "\r\033[K  $BO$GN✔$RE $BO$MG%s... $RE" "$desc"
+    printf "$BO$MG%s$RE\n\n" "done."
+  fi
+}
+
+
+patch_ddlc_py() {
+  if [ ! -f "$dir/DDLC.py" ]; then
+    echo "patch: $dir/DDLC.py: no such file or directory." >&2
+    return 1
+  fi
+
+  cat <<EOF | patch "$dir/DDLC.py" >/dev/null
+52a53
+>     return gamedir + "/saves"
+EOF
+
+}
+error_done_wrap "Patching DDLC.py" patch_ddlc_py
+
+
+if prompt_yes_no "Enable console (Shift+O)?"; then
+  add_console() {
+    cat > "$dir/game/dev_console.rpy" <<EOF
+init python:
+    config.console = True
+EOF
+  }
+  error_done_wrap "Enabling console" add_console
+fi
+
+
+if prompt_yes_no "Install expressions previewer (EXP PREVIEW topic)?"; then
+  install_exp_preview() {
+    curl -sL "https://raw.githubusercontent.com/Monika-After-Story/MonikaModDev/master/Monika%20After%20Story/game/dev/dev_exp_previewer.rpy" > "$dir/game/dev_exp_previewer.rpy"
+  }
+  error_done_wrap "Installing expressions previewer" install_exp_preview
+fi
+
+
+if [ -f ~/".renpy/Monika After Story/persistent" ]; then
+  if prompt_yes_no "Would you like to copy existing persistent into development install?"; then
+      copy_persistent() {
+        mkdir -p "$dir/game/saves"
+        cp ~/".renpy/Monika After Story/persistent" "$dir/game/saves"
+      }
+      error_done_wrap "Copying persistent" copy_persistent
+  fi
+fi
+
+
+printf "  $BO$RD♥$RE $BO$MG%s$RE\n\n" "All done!"
