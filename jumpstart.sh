6b0a3a5632c69ffcab05620dcaf3288e3f78b978
dreamscached <dreamscache.d@gmail.com>
2021-11-05 19:05:30 +0100

    Rename colors

--- jumpstart.sh
+++ jumpstart.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 if [ -t 1 ]; then
   F() { printf "\033[%sm" "$1"; }
-  RE="$(F 0)"
-  BO="$(F 1)"
-  DI="$(F 2)"
-  GN="$(F "38;5;40")"
-  FV="$(F "38;5;129")"
-  RD="$(F "38;5;196")"
-  MG="$(F "38;5;213")"
-  GR="$(F "38;5;239")"
-  LG="$(F "38;5;249")"
+  RE="$(F 0)"          # Reset color and styling
+  BO="$(F 1)"          # Bold
+  DI="$(F 2)"          # Dimmed
+  GN="$(F "38;5;40")"  # Green
+  VT="$(F "38;5;129")" # Violet
+  RD="$(F "38;5;196")" # Red
+  MA="$(F "38;5;213")" # Magenta
+  GY="$(F "38;5;239")" # Grey
+  LY="$(F "38;5;249")" # Light grey
 fi
 
 echo
 echo
-printf "               $BO${MG}Jumpstart, a Monika After Story dev install boostrap tool.$RE\n"
-printf "                           $BO${MG}Made with $BO$RD♥$RE$BO$MG by friends of Monika.$RE\n"
+printf "               $BO${MA}Jumpstart, a Monika After Story dev install boostrap tool.$RE\n"
+printf "                           $BO${MA}Made with $BO$RD♥$RE$BO$MA by friends of Monika.$RE\n"
 echo
 echo
 
 if [ "$#" -lt 1 ]; then
-  printf "  $BO${LG}Usage: $MG$0$RE $BO$GR[${LG}options $GR...] <${MG}command$GR> [${LG}arguments $GR...]$RE\n\n\n"
+  printf "  $BO${LY}Usage: $MA$0$RE $BO$GY[${LY}options $GY...] <${MA}command$GY> [${LY}arguments $GY...]$RE\n\n\n"
   exit 1
 fi
 
@@ -29,12 +29,12 @@ dir="$1"
 
 prompt_yes_no() {
   printf "\n\033[1A"
-  printf "  $BO$FV?$RE $BO$MG%s $RE$BO$GR(Y/n) $RE" "$1"
+  printf "  $BO$VT?$RE $BO$MA%s $RE$BO$GY(Y/n) $RE" "$1"
   read ok
 
   case "$ok" in
     "")
-      printf "\033[1A\r\033[K  $BO$FV?$RE $BO$MG%s $RE$BO$GR(Y/n) ${RE}Y\n" "$1"
+      printf "\033[1A\r\033[K  $BO$VT?$RE $BO$MA%s $RE$BO$GY(Y/n) ${RE}Y\n" "$1"
       return 0;;
     "Y"|"y"|"")
       return 0;;
@@ -46,17 +46,17 @@ prompt_yes_no() {
 
 error_done_wrap() {
   desc="$1"
-  printf "  $BO$LG🛠$RE $BO$MG%s... $RE" "$desc"
+  printf "  $BO$LY🛠$RE $BO$MA%s... $RE" "$desc"
   cmd="$2"
   shift 2
   if ! err="$("$cmd" "$@" 3>&2 2>&1 1>&3)"; then
-    printf "\r\033[K  $BO$RD✘$RE $BO$MG%s... $RE" "$desc"
+    printf "\r\033[K  $BO$RD✘$RE $BO$MA%s... $RE" "$desc"
     printf "$BO${RD}error!\n\n    %s$RE\n\n" "$err"
     printf "  $BO$RD✘ %s$RE\n\n" "Patching failed!"
     exit 1
   else
-    printf "\r\033[K  $BO$GN✔$RE $BO$MG%s... $RE" "$desc"
-    printf "$BO$MG%s$RE\n\n" "done."
+    printf "\r\033[K  $BO$GN✔$RE $BO$MA%s... $RE" "$desc"
+    printf "$BO$MA%s$RE\n\n" "done."
   fi
 }
 
@@ -106,4 +106,4 @@ if [ -f ~/".renpy/Monika After Story/persistent" ]; then
 fi
 
 
-printf "  $BO$RD♥$RE $BO$MG%s$RE\n\n" "All done!"
+printf "  $BO$RD♥$RE $BO$MA%s$RE\n\n" "All done!"
