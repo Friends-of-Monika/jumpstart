2e12cd539066af84ae659995939ebfac82977c28
dreamscached <dreamscache.d@gmail.com>
2021-11-06 09:53:58 +0100

    Change warning text

--- include/autogenerated_warn.chunk.sh
+++ include/autogenerated_warn.chunk.sh
@@ -1,6 +1,7 @@
-#
-# ***   THIS IS AN AUTO-GENERATED FILE!   ***
-# It was built from multiple chunks stored separately.
-# Instead of editing it directly, edit the fragment that was included
-# (see #include comments.)
-#
+# *** HEADS UP! ***
+# This script was auto-generated from separate chunks stored in include/,
+# and you SHOULD NOT edit it directly!
+# For user's convenience, we've prebuilt it and uploaded it into the repository.
+# Whenever you want to submit any changes, EDIT INCLUDED chunks/add more chunks
+# in include/ and build.sh script.
+# Thanks for peeking here!
